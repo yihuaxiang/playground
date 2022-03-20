0d5d7f3d84d3cd5e10317808a2570acd4dd16e61
敖武 <dongdong.fdd@alibaba-inc.com>
2022-03-20 19:31:39 +0800

    start.sh

--- start.sh
+++ start.sh
@@ -4,10 +4,20 @@ pid=$(lsof -i:8080 | grep java | awk '{print $2}')
 
 if [ -z $pid ]
 then
-  echo -z
+  echo 'website is already done'
 else
-  echo not -z
-  echo
+  echo 'website is not done'
+  echo 'try to stop website'
+  kill -9 ${pid}
+  echo 'website already done'
 fi
-#kill -o ${pid}
+
+
+echo begin package
+mvn package
+echo finish package
+
+echo begin start
+nohup java -jar target/website-0.0.1-SNAPSHOT.jar 2&1>/dev/null
+echo success
 
