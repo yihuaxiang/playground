85bb06338165e63e40213e2242d18e184a8c2b16
yihuaxiang <fudongdonguestc@gmail.com>
2022-04-21 23:52:25 +0800

    domain

--- src/main/java/com/fudongdong/website/controller/ImgController.java
+++ src/main/java/com/fudongdong/website/controller/ImgController.java
@@ -30,7 +30,7 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/img")
 @RestController
 @CrossOrigin(origins = {"https://z.wiki", "https://fudongdong.com", "https://www.fudongdong.com", "https://www.weizeling.com",
-    "https://51shazhu.com", "https://playground.fudongdong.com", "http://localhost:8080/"})
+    "https://51shazhu.com", "https://playground.z.wiki", "https://playground.fudongdong.com", "http://localhost:8080/"})
 public class ImgController {
     private final IOssService ossService;
 
