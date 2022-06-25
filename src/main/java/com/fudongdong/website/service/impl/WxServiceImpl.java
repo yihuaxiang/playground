abab973bb6d835e541a0ba7891005da2a98ec6db
yihuaxiang <fudongdonguestc@gmail.com>
2022-06-25 22:16:12 +0800

    feat: 微信公众号api

--- src/main/java/com/fudongdong/website/service/impl/WxServiceImpl.java
+++ src/main/java/com/fudongdong/website/service/impl/WxServiceImpl.java
@@ -1,6 +1,9 @@
 package com.fudongdong.website.service.impl;
 
+import cn.hutool.http.HttpUtil;
 import com.fudongdong.website.service.IWxService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 /**
@@ -8,10 +11,20 @@ import org.springframework.stereotype.Service;
  * @date 2022/6/25 19:49
  */
 @Service
+@Slf4j
 public class WxServiceImpl implements IWxService {
+
+    @Value("${yidaoyilu.wx.appId}")
+    private String wxAppId;
+    @Value("${yidaoyilu.wx.appSecret}")
+    private String wxAppSecret;
+
     @Override
     public String getAccessToken() {
-        return null;
+        String getTokenUrl = "https://api.weixin.qq.com/cgi-bin/token";
+        String url = String.format("%s?grant_type=client_credential&appid=%s&secret=%s", getTokenUrl, this.wxAppId, this.wxAppSecret);
+        log.info("getAccessToken, url is {}", url);
+        return HttpUtil.get(url);
     }
 
     @Override
